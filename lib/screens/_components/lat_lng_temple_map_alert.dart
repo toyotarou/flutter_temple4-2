56536d1cfb8fec79a8b81db109bbec4d7fb8b3d0
Toyoda Hideyuki <hide.toyoda@gmail.com>
2024-07-03 08:06:32 +0900

    commit 20240703

--- lib/screens/_components/lat_lng_temple_map_alert.dart
+++ lib/screens/_components/lat_lng_temple_map_alert.dart
@@ -185,7 +185,10 @@ class _LatLngTempleDisplayAlertState
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Container(),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.train),
+                  ),
                   IconButton(
                     onPressed: () {
                       mapController.move(currentCenter, 13);
